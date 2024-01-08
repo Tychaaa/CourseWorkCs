070b9e754faecd8e7c48fd3859a2a412d7d395a8
Shevtsov Artem <Shevcov_2k4_2@mail.ru>
2024-01-08 19:27:27 +0700

    Создал класс Magic

---
+++ Magic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWorkCs
+{
+    public class Magic : Arsenal
+    {
+        // Конструктор по умолчанию
+        public Magic() : base()
+        {
+            // Вызов конструктора базового класса Arsenal
+        }
+
+        // Конструктор с параметрами
+        public Magic(string n, int dmg, int cst) : base(n, dmg, cst)
+        {
+            // Вызов конструктора базового класса Arsenal с параметрами
+        }
+    }
+}
