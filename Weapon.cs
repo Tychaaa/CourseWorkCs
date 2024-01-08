e70ffa80f160ebee06349b96f6ce87a3edaef313
Shevtsov Artem <Shevcov_2k4_2@mail.ru>
2024-01-08 19:24:54 +0700

    Создал класс Weapon

---
+++ Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWorkCs
+{
+    public class Weapon : Arsenal
+    {
+        // Конструктор по умолчанию
+        public Weapon() : base()
+        {
+            // Вызов конструктора базового класса Arsenal
+        }
+
+        // Конструктор с параметрами
+        public Weapon(string n, int dmg, int cst) : base(n, dmg, cst)
+        {
+            // Вызов конструктора базового класса Arsenal с параметрами
+        }
+    }
+}
